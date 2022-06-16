f05ecd35d63ea4b22a0987a11343f21c088832bb
Vadim Kasatkin <penguin2804@gmail.com>
2022-06-16 14:36:24 +0400

    feat: base pages

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, contacts, about
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('contacts/', contacts, name="contacts"),
+    path('about/', about, name="about"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "main/base.html")
+    return render(request, "main/main.html")
+
+
+def about(request):
+    return render(request, "main/about.html")
+
+
+def contacts(request):
+    return render(request, "main/contacts.html")
